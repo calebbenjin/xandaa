0238b4adf5d312ac3972f59a36d6bbe81892dc95
kaiylebdev <calebben94@gmail.com>
2020-09-05 23:39:17 +0100

    reg form bug fix

--- js/app.js
+++ js/app.js
@@ -1,3 +1,50 @@
-$(document).ready(function() {
-  $('select').niceSelect();
-});
+// $(document).ready(function() {
+//   $('select').niceSelect();
+// });
+
+
+// Data Module
+const DataController = (() => {
+
+})();
+
+// UI Controller
+const UIController = (() => {
+
+  const DOMStrings = {
+    closeToggle: '.toggle-close',
+    showToggle: '.bugger-show'
+  }
+
+  return {
+    getDomStrings: () => {
+      return {
+        closeBtn: document.querySelector(DOMStrings.closeToggle),
+        showBtn: document.querySelector(DOMStrings.showToggle)
+      }
+    },
+
+    DomStrings: () => {
+      return DOMStrings;
+    }
+  }
+
+
+})();
+
+// Global App Controller
+const Controller = ((dataCtrl, UICtrl) => {
+
+  const btnToggle = () => {
+    alert('Hello it works');
+  }
+
+  let DOM = UICtrl.DomStrings();
+  console.log(DOM);
+
+  document.querySelector(DOM.showToggle).addEventListener('click', btnToggle);
+
+  
+
+
+})(DataController, UIController);
