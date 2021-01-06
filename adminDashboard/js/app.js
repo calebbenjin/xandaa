c8eb29778b48f93cb1dce6359178830db6feb1e9
calebbenjin <calebben94@gmail.com>
2021-01-06 06:29:38 +0100

    imgs deleted

--- adminDashboard/js/app.js
+++ adminDashboard/js/app.js
@@ -1,4 +1,5 @@
 
+
 // Users Dropdown
 document.querySelector('#dropdown-nav').addEventListener('click', function() {
   document.querySelector('.dropdown-list').classList.toggle('show-users')
@@ -8,4 +9,10 @@ document.querySelector('#dropdown-nav').addEventListener('click', function() {
 // Pages Dropdown
 document.querySelector('#page-dropdown').addEventListener('click', function() {
   document.querySelector('.page-list').classList.toggle('show-page')
+})
+
+
+// Finance Dropdown
+document.querySelector('#dropdown-finance').addEventListener('click', function() {
+  document.querySelector('.finance-list').classList.toggle('show-finance')
 })
